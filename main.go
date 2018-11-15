c38dd85701891912df560ab9c962696861759ba0
vyrwu <alek_nowak@hotmail.com>
2018-11-15 15:20:27 +0100

    Fix conn reset by peer

--- main.go
+++ main.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
+	"github.com/iafoosball/auth-service/jwt"
 	"github.com/iafoosball/auth-service/rs256"
+	"github.com/iafoosball/auth-service/social"
+	"net/http"
+	"os"
 )
 
 func main() {
@@ -10,13 +15,13 @@ func main() {
 	if err := rs256.MakeRSAKeysToDisk("test"); err != nil {
 		fmt.Println(err)
 	}
-	//defer fmt.Println("Exited....")
-	//r := mux.NewRouter()
-	//r = jwt.SetRoutes(r)
-	//r = social.SetRoutes(r)
-	//var addr string
-	//if addr = os.Getenv("AUTH_ADDR"); addr == "" {
-	//	addr = "localhost:8001"
-	//}
-	//http.ListenAndServe(addr, r)
+	defer fmt.Println("Exited....")
+	r := mux.NewRouter()
+	r = jwt.SetRoutes(r)
+	r = social.SetRoutes(r)
+	var addr string
+	if addr = os.Getenv("AUTH_ADDR"); addr == "" {
+		addr = "localhost:8001"
+	}
+	http.ListenAndServe(addr, r)
 }
