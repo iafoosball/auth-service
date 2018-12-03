731cef3e87311e6c68e6488a5269259d3c90ffa3
vyrwu <alek_nowak@hotmail.com>
2018-12-03 18:50:04 +0100

    Create basic-auth route

--- delete-kong.sh
+++ delete-kong.sh
@@ -9,10 +9,17 @@ ADMIN_PORT=8050
 ADMIN_ADDR=$ADMIN_HOSTNAME:$ADMIN_PORT
 
 # Get route id on Service
-ROUTE_ID=`curl -s "http://$ADMIN_ADDR/services/$API-service/routes" | jq ".data[].id" | tr -d \" `
+ROUTE_ID1=`curl -s "http://$ADMIN_ADDR/services/$API-service/routes" | jq ".data[0].id" | tr -d \" `
+ROUTE_ID2=`curl -s "http://$ADMIN_ADDR/services/$API-service/routes" | jq ".data[1].id" | tr -d \" `
 # Delete target on service
-if [ "$ROUTE_ID" != "" ] ; then
-    curl -i -X DELETE "http://$ADMIN_ADDR/routes/$ROUTE_ID"
+if [ "$ROUTE_ID1" != "" ] ; then
+    curl -i -X DELETE "http://$ADMIN_ADDR/routes/$ROUTE_ID1"
+else
+    echo "Route to $API-service was not found"
+fi
+
+if [ "$ROUTE_ID2" != "" ] ; then
+    curl -i -X DELETE "http://$ADMIN_ADDR/routes/$ROUTE_ID2"
 else
     echo "Route to $API-service was not found"
 fi
