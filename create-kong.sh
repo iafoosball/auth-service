63a58317ccf17bf219e5d850007ea8c298020993
vyrwu <alek_nowak@hotmail.com>
2018-12-03 19:05:28 +0100

    Fix route adding

--- create-kong.sh
+++ create-kong.sh
@@ -12,16 +12,18 @@ ADMIN_ADDR=$ADMIN_HOSTNAME:$ADMIN_PORT
 curl -i -X POST "http://$ADMIN_ADDR/services/" --header 'Content-Type: application/json' \
      --data '{ "name": "'$API'-service", "host": "'$API'-service", "port": '$ADMIN_PORT', "protocol": "http", "path": "/" }'
 
-# Create Routes to service
+# Create Route to login
 curl -i -X POST "http://$ADMIN_ADDR/services/$API-service/routes" --header 'Content-Type: application/json' \
-     --data '{ "paths": ["/oauth/login"] }'
+     --data '{ "paths": ["/oauth/login"], "strip_path": false }'
 
-# Get ID of created login route
+# Get ID of created login route and register basic-auth on it
 ROUTE_ID=`curl -s "http://$ADMIN_ADDR/services/$API-service/routes" | jq ".data[].id" | tr -d \" `
-# Register basic-auth plugin
 curl -i -X POST "http://$ADMIN_ADDR/routes/$ROUTE_ID/plugins" \
      --data 'name=basic-auth'
+
+# Create Route for other endpoints
 curl -i -X POST "http://$ADMIN_ADDR/services/$API-service/routes" --header 'Content-Type: application/json' \
+     --data '{ "methods": ["GET", "POST"], "strip_path": false  }'
 
 # Add UPSTREAM
 curl -i -X POST "http://$ADMIN_ADDR/upstreams/" --header 'Content-Type: application/json'  \
